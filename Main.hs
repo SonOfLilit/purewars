e354f10d589a8782154f30110877f82920a843b8
Aur Saraf <sonoflilit@gmail.com>
2010-08-07 00:40:52 +0300

    Initial: framework for a vector graphics game

---
+++ Main.hs
@@ -0,0 +1,84 @@
+module Main where
+import Prelude hiding (lines)
+import Control.Monad
+import Control.Monad.State
+import Data.IORef
+import Data.Time.Clock
+import Graphics.Rendering.OpenGL (($=))
+import qualified Graphics.Rendering.OpenGL as GL
+import qualified Graphics.UI.GLUT as GLUT
+
+type Lines = [(GL.GLfloat, GL.GLfloat, GL.GLfloat, GL.GLfloat)]
+data GameState = GameState {x, y :: GL.GLfloat}
+type LogicStep = NominalDiffTime -> State GameState ()
+
+logic :: LogicStep
+logic t = do
+  state <- get
+  let d = 0.5 * realToFrac t
+  put state {x = x state + 1*d, y = y state + 2*d}
+
+getLines :: GameState -> Lines
+getLines GameState {x = x, y = y} = [
+  (x, y, 0.0, 30.0),
+  (0.0, 30.0, 30.0, 30.0),
+  (30.0, 30.0, x, y)]
+
+drawOneLine :: GL.Vertex2 GL.GLfloat -> GL.Vertex2 GL.GLfloat -> IO ()
+drawOneLine p1 p2 = GL.renderPrimitive GL.Lines $ do GL.vertex p1; GL.vertex p2
+
+drawLines :: Lines -> IO ()
+drawLines ls = do
+  GL.color (GL.Color3 1.0 1.0 1.0 :: GL.Color3 GL.GLfloat)
+  forM_ ls (\ (x0, y0, x1, y1) ->  
+                drawOneLine (GL.Vertex2 x0 y0) (GL.Vertex2 x1 y1))
+
+initGL :: IO ()
+initGL = do
+  GL.clearColor $= GL.Color4 0 0 0 0
+  GL.shadeModel $= GL.Flat
+  GL.depthFunc $= Nothing
+
+reshape :: GLUT.ReshapeCallback
+reshape size@(GL.Size w h) = do
+  GL.viewport $= (GL.Position 0 0, size)
+  GL.matrixMode $= GL.Projection
+  GL.loadIdentity
+  GL.ortho2D 0 (fromIntegral w) 0 (fromIntegral h)
+
+fps = 1/25
+
+frame :: UTCTime -> IORef GameState -> LogicStep -> GLUT.TimerCallback
+frame lastFrameTime stateRef logicStep = do
+  state <- readIORef stateRef
+  let ((), state') = runState (logicStep 1.0) state
+  writeIORef stateRef state'
+  GLUT.postRedisplay Nothing
+  
+  now <- getCurrentTime
+  let nextFrameTime = fps `addUTCTime` lastFrameTime
+      waitTime = nextFrameTime `diffUTCTime` now
+      msWait = truncate (waitTime * 1000)
+  GLUT.addTimerCallback msWait (frame now stateRef logicStep)
+
+display :: IORef GameState -> GLUT.DisplayCallback
+display stateRef = do
+  state <- readIORef stateRef
+  
+  GL.clear [GL.ColorBuffer]
+  drawLines $ getLines state
+  GLUT.swapBuffers
+
+main :: IO ()
+main = do
+  _ <- GLUT.getArgsAndInitialize
+  GLUT.initialDisplayMode $= [GLUT.DoubleBuffered, GLUT.RGBMode]
+  GLUT.initialWindowSize $= GL.Size 640 480
+  _ <- GLUT.createWindow "purewars"
+  initGL
+  GLUT.reshapeCallback $= Just reshape
+  now <- getCurrentTime
+  stateRef <- newIORef GameState {x = 0, y = 8}
+  GLUT.displayCallback $= display stateRef
+  GLUT.addTimerCallback 1 (frame now stateRef logic)
+  GLUT.mainLoop

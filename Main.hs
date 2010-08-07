af1670a4f13d9ec0b91d8cbf7f080bfabac5e922
Aur Saraf <sonoflilit@gmail.com>
2010-08-07 05:29:01 +0300

    Vector operations

--- Main.hs
+++ Main.hs
@@ -8,30 +8,69 @@ import Graphics.Rendering.OpenGL (($=))
 import qualified Graphics.Rendering.OpenGL as GL
 import qualified Graphics.UI.GLUT as GLUT
 
-type Lines = [(GL.GLfloat, GL.GLfloat, GL.GLfloat, GL.GLfloat)]
-data GameState = GameState {x, y :: GL.GLfloat}
+type Length = Double
+type Vector2 = (Length, Length)
+($*^) :: Length -> Vector2 -> Vector2
+s $*^ (x, y) = (s*x, s*y)
+(^+^) :: Vector2 -> Vector2 -> Vector2
+(a, b) ^+^ (a', b') = (a+a', b+b')
+
+type Matrix2 = (Vector2, Vector2)
+($*#) :: Length -> Matrix2 -> Matrix2
+s $*# (u, v) = (s $*^ u, s $*^ v)
+(#*^) :: Matrix2 -> Vector2 -> Vector2
+(v, u) #*^ (x, y) = (x $*^ v) ^+^ (y $*^ u)
+(#*#) :: Matrix2 -> Matrix2 -> Matrix2
+m #*# (c, d) = (m #*^ c, m #*^ d)
+type Transformation = (Matrix2, Vector2)
+(~*~) :: Transformation -> Transformation -> Transformation
+(m, v) ~*~ (m', v') = (m #*# m', v ^+^ v')
+(~*^) :: Transformation -> Vector2 -> Vector2
+(m, u) ~*^ v = (m #*^ v) ^+^ u
+zeroV = (0, 0)
+idM = ((1, 0), (0, 1))
+
+scale k = (k $*# idM, zeroV)
+translate v = (idM, v)
+
+type Line = (Vector2, Vector2)
+data GameState = GameState {ship1 :: Ship}
 type LogicStep = NominalDiffTime -> State GameState ()
+
+class GameObject a where
+  tick :: a -> NominalDiffTime -> IO a
+  draw :: a -> [Line]
+
+data Ship = Ship Vector2
+instance GameObject Ship where
+  tick (Ship (x, y)) t = return $ Ship (x+t', y+2*t') where t' = realToFrac t
+  draw (Ship pos) = map t2 shipShape
+    where t = (scale 10 ~*~ translate pos ~*^)
+          t2 (a, b) = (t a, t b)
+
+shipShape :: [Line]
+shipShape = [((-1, -1), (1, 0))
+            ,((1, 0), (-1, 1))
+            ,((-1, 1), (-1, -1))]
 
 logic :: LogicStep
 logic t = do
   state <- get
   let d = 0.5 * realToFrac t
-  put state {x = x state + 1*d, y = y state + 2*d}
+      Ship pos = ship1 state
+  put state {ship1 = Ship (pos ^+^ (d, 2*d))}
 
-getLines :: GameState -> Lines
-getLines GameState {x = x, y = y} = [
-  (x, y, 0.0, 30.0),
-  (0.0, 30.0, 30.0, 30.0),
-  (30.0, 30.0, x, y)]
+getLines :: GameState -> [Line]
+getLines GameState {ship1 = ship} = draw ship
 
-drawOneLine :: GL.Vertex2 GL.GLfloat -> GL.Vertex2 GL.GLfloat -> IO ()
+drawOneLine :: GL.Vertex2 Length -> GL.Vertex2 Length -> IO ()
 drawOneLine p1 p2 = GL.renderPrimitive GL.Lines $ do GL.vertex p1; GL.vertex p2
 
-drawLines :: Lines -> IO ()
-drawLines ls = do
+drawLines :: [Line] -> IO ()
+drawLines lines = do
   GL.color (GL.Color3 1.0 1.0 1.0 :: GL.Color3 GL.GLfloat)
-  forM_ ls (\ (x0, y0, x1, y1) ->  
-                drawOneLine (GL.Vertex2 x0 y0) (GL.Vertex2 x1 y1))
+  forM_ lines (\ (p0, p1) -> drawOneLine (v p0) (v p1))
+    where v pos = uncurry GL.Vertex2 $ pos
 
 initGL :: IO ()
 initGL = do
@@ -78,7 +117,7 @@ main = do
   initGL
   GLUT.reshapeCallback $= Just reshape
   now <- getCurrentTime
-  stateRef <- newIORef GameState {x = 0, y = 8}
+  stateRef <- newIORef GameState {ship1 = Ship (0, 0)}
   GLUT.displayCallback $= display stateRef
   GLUT.addTimerCallback 1 (frame now stateRef logic)
   GLUT.mainLoop
